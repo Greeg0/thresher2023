ad1762422de6e356a24afae9cc1894a70fd833d5
Duckfeld-McJimmers <ivan.desc@gmail.com>
2023-03-27 22:42:29 -0700

    Fixed the display bugs, reduced some stuff to a function, added a delay to regulate the loop, but otherwise doesn't effect the functionality in any noticeable scale. there is a bit of jittering with the servo potentiometer setup causing unnessary display updates and servo buzzing, this is the only remaining bug.

--- thresher2023.ino
+++ thresher2023.ino
@@ -1,20 +1,27 @@
 #include <LiquidCrystal.h>
 #include <Bounce2.h> // used to toggle button
 #include <Servo.h>
+
+
+
 #define buttonPin 8 // Assuming the joyButton pin is attached to pin7
 
-Bounce button = Bounce(); // Create object bounce.
+Bounce button = Bounce(); // Create object bounce. This is used to detect a button press.
 
-bool state;
+bool state; 
+
+
+
 
 const int SERVO = 12; // Assuming servo is attached to pin12
 const int joyVer = A1; // Vertical output to in1
 const int joyHor = A2; // Horizontal output to in2
 
 Servo myServo; // name of servo
-Servo myServo2;
-LiquidCrystal lcd(2, 3, 4, 13, 6, 7);
+
+LiquidCrystal lcd(2, 3, 4, 13, 6, 7); //initialize display, 13 was just used as the pwm of pin 5 was needed for the motor control.
 bool isUpdated = true; // the display should not be updated unless it's necessary or a mess of characters happen.
+
 int val = 0; // initial value
 
 const int analogInPin = A0; // potentiometer
@@ -34,24 +41,24 @@ int prevConClear = 0;
 int updateSpeed = 1; // This is the speed that the speeds will update at. So if the update speed is too slow, change this.
 
 void setup() {
-  Serial.begin(9600); // Begin serial monitor used to test sensors with Serial.println(foobar)
-  lcd.begin(16,2);
-  pinMode(motorPin, OUTPUT);
-  pinMode(fanPin, OUTPUT);  
-  myServo.attach(SERVO); // attach servo to servo object.
+	lcd.begin(16,2);
+	pinMode(motorPin, OUTPUT);
+	pinMode(fanPin, OUTPUT);  
+	myServo.attach(SERVO); // attach servo to servo object.
 
-  pinMode(buttonPin, INPUT_PULLUP);
-  button.attach(buttonPin); // Assign the button to the bounce object.
+	pinMode(buttonPin, INPUT_PULLUP);
+	button.attach(buttonPin); // Assign the button to the bounce object.
 
-  pinMode(joyVer, INPUT);
-  pinMode(joyHor, INPUT);
+	pinMode(joyVer, INPUT);
+	pinMode(joyHor, INPUT);
 
-  pinMode(analogInPin, INPUT);
+	pinMode(analogInPin, INPUT);
 
   // initalise state to false, which will start on standby.
   state = false;
 }
 
+// loop, runs either standby() or regular() depending on state.
 void loop() {
     if (getButton()) { // get if button is pressed
     state = !state;     // if true, swap state
@@ -83,17 +90,48 @@ void standby() {
 		lcd.print("OFF");
 		isUpdated = false;
 	}
-	Serial.println(analogRead(joyVer));
+}
+
+// This function will return the cursor position to display and will update the unused left digit to 0.
+int displayCursor(const int& basePos, const int& inVal){
+	int outVal;
+	if (inVal % 10 != inVal){
+		outVal = basePos;
+	} else {
+		outVal = basePos + 1;
+		lcd.setCursor(basePos, 1); // UPDATE UNUSED DIGIT TO 0.
+		lcd.print(0);
+	}
+	return outVal;
 }
 
 void updateDisplay(){
-	// This is what is updated when the adjustments update.
-	lcd.setCursor(2,1);
-	lcd.print(map(drumSpeed, 0, maxSpeed, 0, 99));
-	lcd.setCursor(7,1);
-	lcd.print(map(fanSpeed, 0, maxSpeed, 0, 99));
-	lcd.setCursor(12,1);
-	lcd.print(map(conClear, 0, 179, 0, 99));
+	// This is what is updated when the adjustments update. 
+	val = map(drumSpeed, 0, maxSpeed, 0, 99);
+	lcd.setCursor( displayCursor(2, val) , 1);
+	lcd.print(val);
+	val = map(fanSpeed, 0, maxSpeed, 0, 99);
+	lcd.setCursor( displayCursor(7, val) , 1);
+	lcd.print(val);
+	val = map(conClear, 0, 179, 0, 99);
+	lcd.setCursor( displayCursor(12, val), 1);
+	lcd.print(val);
+}
+
+// This is a function that takes a speed, an input from a joystick, and outputs the adjusted speed to a pin. The resting value of the joystick is around 500 so 400 and 600 are a good simple range to use.
+void speedUpdate(int& speed, const int& inPin, const int& outPin){
+ 
+	sensorValue=analogRead(inPin);
+
+	// Adjust speed
+	if (sensorValue > 600 && speed <= maxSpeed-updateSpeed){ // If shiftUp, increase speed. If the next update will make it go over the max speed, do not update. 
+		speed += updateSpeed;
+		updateDisplay();
+	} else if (sensorValue < 400 && speed >= updateSpeed){ // If shiftDown, decrease speed. If the next update will make it go below 0, do not update.
+		speed -= updateSpeed;
+		updateDisplay();
+	}
+	analogWrite(outPin, speed);
 }
 
 void regular(){
@@ -106,43 +144,20 @@ void regular(){
 		lcd.print("F:");
 		lcd.setCursor(12,0);
 		lcd.print("C:");
-		lcd.setCursor(5,1);
+		lcd.setCursor(4,1);
 		lcd.print("%");
-		lcd.setCursor(10,1);
+		lcd.setCursor(9,1);
 		lcd.print("%");
-		lcd.setCursor(15,1);
+		lcd.setCursor(14,1);
 		lcd.print("%");
 		updateDisplay();
+		isUpdated = false;
 	}
-	
-	// I decided to break these down in seperate functions to diagnose issues, but may revert back to the old function after its tested, but probably not.
 
-	sensorValue=analogRead(joyVer);
+	speedUpdate(drumSpeed, joyVer, motorPin);
+	speedUpdate(fanSpeed, joyHor, fanPin);
 
-	// Adjust motorspeed
-	if (sensorValue > 600 && drumSpeed <= maxSpeed-updateSpeed){ // If shiftUp, increase speed. If the next update will make it go over the max speed, do not update. 
-		drumSpeed += updateSpeed;
-		updateDisplay();
-	} else if (sensorValue < 400 && drumSpeed >= updateSpeed){ // If shiftDown, decrease speed. If the next update will make it go below 0, do not update.
-		drumSpeed -= updateSpeed;
-		updateDisplay();
-	}
-	analogWrite(motorPin, drumSpeed);
-	
-	sensorValue=analogRead(joyHor);
-	
-	// Adjust fanspeed
-	if (sensorValue > 600 && fanSpeed <= maxSpeed-updateSpeed){ // If shiftUp, increase speed. If the next update will make it go over the max speed, do not update. 
-		fanSpeed += updateSpeed;
-		updateDisplay();
-	} else if (sensorValue < 400 && fanSpeed >= updateSpeed){ // If shiftDown, decrease speed. If the next update will make it go below 0, do not update.
-		fanSpeed -= updateSpeed;
-		updateDisplay();
-	}
-	analogWrite(fanPin, fanSpeed);
-
-
-	// updates servos, if both servos will be adjusted simutaneously, they can be both controlled by the same pin (i think).
+	// updates servos, if both servos will be adjusted simutaneously, they can be both controlled 
 	sensorValue = analogRead(analogInPin);
 	conClear = map(sensorValue, 0, 1023, 0, 179);
 	myServo.write(conClear);
@@ -150,4 +165,6 @@ void regular(){
 		updateDisplay();
 	}
 	prevConClear = conClear;
+
+	delay(10); // No rush.
 }
